b537b14f80a9d20f29f6c8a7db098ec9ad456a54
insub <j.j.kant1948@gmail.com>
2024-10-01 16:34:23 +0900

    change language-option css to follow scrolling, add indent

--- new_main_page/srcs/js/pages/game-select.js
+++ new_main_page/srcs/js/pages/game-select.js
@@ -6,15 +6,15 @@ export function gameSelect(currentLanguage) {
 	appContainer.innerHTML = `
 	<h1 id="welcome-title" class="title-text">${languages[currentLanguage].welcome}</h1>
 	<div class="button-container" style="margin-bottom: 2vw;">
-	<button id="2d-pong-btn" class="button">
-	<img src="./img/game_select/2d-pong.png" alt="2d image">
-	<span id="2d-pong" class="text">${languages[currentLanguage].twoDPong}</span>
-	</button>
+		<button id="2d-pong-btn" class="button">
+			<img src="./img/game_select/2d-pong.png" alt="2d image">
+			<span id="2d-pong" class="text">${languages[currentLanguage].twoDPong}</span>
+		</button>
 
-	<button id="3d-pong-btn" class="button2">
-	<img src="./img/game_select/3d-pong.png" alt="3d image">
-	<span id="3d-pong" class="text">${languages[currentLanguage].threeDPong}</span>
-	</button>
+		<button id="3d-pong-btn" class="button2">
+			<img src="./img/game_select/3d-pong.png" alt="3d image">
+			<span id="3d-pong" class="text">${languages[currentLanguage].threeDPong}</span>
+		</button>
 	</div>
 	`
 	const twoDPongSelBtn = document.getElementById('2d-pong-btn');
