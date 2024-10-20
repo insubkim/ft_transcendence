828de6962ab97efdfc3ceb3f413bd8f2a6f8b2c1
jungjkim <jjkimfplc@gmail.com>
2024-10-20 15:55:45 +0900

    edit virtual env

--- BE/game_service/pong/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('start/', views.start_game, name='start_game'),
-]
--- BE/game_service/pong/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import JsonResponse
-
-def start_game(request):
-    # Add logic to start the game
-    return JsonResponse({"message": "Game started successfully"}, status=200)
