b9b79b13512e0d7f5521428efa432cb6aaf5fc3c
yusekim <youwin0802@naver.com>
2024-10-29 16:44:41 +0900

    index.html path update

--- FE/srcs/js/pages/2d-mode-select.js
+++ FE/srcs/js/pages/2d-mode-select.js
@@ -7,12 +7,12 @@ export function twoDModeSel(currentLanguage) {
 	<h1 id="2d-mode-select-title" class="title-text">${languages[currentLanguage].gameModeSel}</h1>
 	<div class="button-container">
 		<button id="two-1V1-btn" class="button">
-			<img src="./img/mode_select/fist-bump.png">
+			<img src="./FE/img/mode_select/fist-bump.png">
 			<span id="2D-1V1" class="text">${languages[currentLanguage].oneVone}</span>
 		</button>
 
 		<button id="two-tournament-btn" class="button2">
-			<img src="./img/mode_select/Trophy-icon.png">
+			<img src="./FE/img/mode_select/Trophy-icon.png">
 			<span id="2D-tournament" class="text">${languages[currentLanguage].tournament}</span>
 		</button>
 	</div>
