e47228c44b600c72628c63ab6dd0ae38f6bca49a
insub <j.j.kant1948@gmail.com>
2024-10-30 21:40:45 +0900

    feat: apply tls to frontend container

---
+++ FE/tools/start.sh
@@ -0,0 +1,5 @@
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+         -keyout /etc/nginx/cert.key -out /etc/nginx/cert.crt \
+         -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost";
+
+nginx -g 'daemon off;'
