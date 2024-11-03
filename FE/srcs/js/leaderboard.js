f7c29f97834504492ade95c577afab1bd4ee207c
insub <j.j.kant1948@gmail.com>
2024-11-03 15:06:51 +0900

    fixing: leaderboard

--- FE/srcs/js/leaderboard.js
+++ FE/srcs/js/leaderboard.js
@@ -1,26 +1,26 @@
-const top3 = [
-   { "2d_1vs1": ['a', 'b', 'c'], win: [3, 2, 1], total: [6, 4, 2] },
-   { "2d_tournament": ['c', 'd', ''], win: [5, 3, ''], total: [8, 5, ''] },
-   { "3d_1vs1": ['e', 'f', ''], win: [4, 2, ''], total: [7, 4, ''] },
-   { "3d_tournament": ['g', 'h', ''], win: [6, 4, ''], total: [10, 7, ''] }
-];
+// const top3 = [
+//    { "2d_1vs1": ['a', 'b', 'c'], win: [3, 2, 1], total: [6, 4, 2] },
+//    { "2d_tournament": ['c', 'd', ''], win: [5, 3, ''], total: [8, 5, ''] },
+//    { "3d_1vs1": ['e', 'f', ''], win: [4, 2, ''], total: [7, 4, ''] },
+//    { "3d_tournament": ['g', 'h', ''], win: [6, 4, ''], total: [10, 7, ''] }
+// ];
 
 //위와 같은 자료구조로 온다 가정하고 로직짬
 //fetch로 받을경우 아래와 같은 함수 사용예정
 
-// let top3 = []; // 데이터를 저장할 변수를 let으로 선언
+let top3 = []; // 데이터를 저장할 변수를 let으로 선언
 
-// async function fetchTop3Data() {
-//     try {
-//         const response = await fetch('https://localhost/data.json'); // 로컬 JSON 파일 경로 또는 서버 URL 사용
-//         if (!response.ok) throw new Error('네트워크 응답 오류');
-//         top3 = await response.json();
-//         console.log(top3); // 데이터 확인용 로그
-//         updateLeaderboard(top3);
-//     } catch (error) {
-//         console.error('데이터 가져오기 오류:', error);
-//     }
-// }
+export async function fetchTop3Data() {
+    try {
+        const response = await fetch('https://127.0.0.1:8000/api/game-rankings'); // 로컬 JSON 파일 경로 또는 서버 URL 사용
+        if (!response.ok) throw new Error('네트워크 응답 오류');
+        top3 = await response.json();
+        console.log('fetchTop3Data' + top3); // 데이터 확인용 로그
+        updateLeaderboard(top3);
+    } catch (error) {
+        console.error('데이터 가져오기 오류:', error);
+    }
+}
 
 const container = document.createElement("div");
 container.classList.add("leaderboard-container");
@@ -58,7 +58,7 @@ function updateLeaderboard(modeKey, modeIndex, listId) {
     const modeData = top3[modeIndex];
 
     for (let i = 0; i < 3; i++) {
-        if (modeData[modeKey][i]) {  // 이름이 있는 경우에만 표시
+        if (modeData && modeData[modeKey] && modeData[modeKey][i]) {  // 이름이 있는 경우에만 표시
             const playerDiv = document.createElement('div');
             playerDiv.classList.add('player');
 
